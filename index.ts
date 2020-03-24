2d80ae42e64fc693182416d43e892f3edd46fcd2
Raine Virta <raine.virta@gmail.com>
2020-03-24 13:31:15 +0200

    update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,40 @@
 import * as t from 'io-ts'
-import { Parser, Response, Route, route, URL } from 'typera-express'
+import { Parser, Response, Route, route } from 'typera-express'
 
-const test: Route<
-  Response.Ok | Response.NotFound | Response.BadRequest<string>
-> = route('get', '/test/', URL.str('id'))(Parser.query(t.any))((req) => {
-  return req.routeParams.id !== 'foo'
-    ? Response.notFound()
-    : Promise.resolve().then(() => Response.ok())
+// works
+const test1: Route<
+  Response.Ok<string> |
+  Response.BadRequest<string>
+> = route('get', '/test')(Parser.query(t.any))((_req) => {
+  return Response.ok('OK')
+})
+
+// works
+const test2: Route<
+  Response.Ok<'OK'> |
+  Response.BadRequest<string>
+> = route('get', '/test')(Parser.query(t.any))((_req) => {
+  return Response.ok('OK')
+})
+
+
+// index.ts:24:7 - error TS2322: Type 'Route<ExpressContext, Response<400, string, undefined> | Response<200, string, undefined>>' is not assignable to type 'Route<ExpressContext, Response<400, string, undefined> | Response<200, "OK", undefined>>'.
+//   Type 'Response<400, string, undefined> | Response<200, string, undefined>' is not assignable to type 'Response<400, string, undefined> | Response<200, "OK", undefined>'.
+//     Type 'Response<200, string, undefined>' is not assignable to type 'Response<400, string, undefined> | Response<200, "OK", undefined>'.
+//       Type 'Response<200, string, undefined>' is not assignable to type 'Response<400, string, undefined>'.
+//         Type '200' is not assignable to type '400'.
+const test3: Route<
+  Response.Ok<'OK'> |
+  Response.BadRequest<string>
+> = route('get', '/test')(Parser.query(t.any))(async (_req) => {
+                                            // ^--- add/remove async
+  return Response.ok('OK')
+})
+
+// works
+const test4: Route<
+  Response.Ok<string> |
+  Response.BadRequest<string>
+> = route('get', '/test')(Parser.query(t.any))(async (_req) => {
+  return Response.ok('OK')
 })
